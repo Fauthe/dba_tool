7f12c06bf771f99011b869810d41c0bc4e3c6f5f
fauthe <manishlama731@gmail.com>
2023-01-18 15:49:24 +0545

    Add project files.

---
+++ dba_tool/DBconnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace dba_tool
+{
+	public class DBconnection
+	{
+		public string sql;
+		public void OnGet()
+		{
+			try
+			{
+				String connectionString = "Data Source=DESKTOP-FM5935J\\TESTNODE; Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					SqlCommand cmd = new SqlCommand(sql, connection);
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+	}
+}
---
+++ dba_tool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dba_tool.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
