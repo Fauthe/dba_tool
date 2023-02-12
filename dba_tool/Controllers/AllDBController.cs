670872a6c95d55dcccb37f3827ae55e869dfe847
Fauthe <manishlama731@gmail.com>
2023-02-12 23:09:05 +0545

    Dashboard halfway

--- dba_tool/Controllers/AllDBController.cs
+++ dba_tool/Controllers/AllDBController.cs
@@ -1,6 +1,8 @@
 ﻿using dba_tool.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Drawing;
 
 namespace dba_tool.Controllers
 {
@@ -38,6 +40,9 @@ namespace dba_tool.Controllers
 		public IActionResult Dashboard(string selectedDB)
 		{
 			ViewBag.SelectedDB = selectedDB;
+			ViewBag.tableCount = GetTableCount(selectedDB);
+			ViewBag.viewCount = GetViewsCount(selectedDB);
+			ViewBag.indexCount = GetIndexesCount(selectedDB);
 			return View();
 		}
 
@@ -62,6 +67,66 @@ namespace dba_tool.Controllers
 					});
 				}
 				
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+
+			}
+		}
+
+		public int GetTableCount(string dbname)
+		{
+			string result = "";
+			try
+			{
+				SqlCommand cmd = new SqlCommand("udp_getTableCount");
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@dbname", dbname);
+				cmd.Connection = DBconnection.DBConnect();
+				result = cmd.ExecuteScalar().ToString();
+				return int.Parse(result);
+
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+
+			}
+		}
+
+		public int GetViewsCount(string dbname)
+		{
+			string result = "";
+			try
+			{
+				SqlCommand cmd = new SqlCommand("udp_getViewsCount");
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@dbname", dbname);
+				cmd.Connection = DBconnection.DBConnect();
+				result = cmd.ExecuteScalar().ToString();
+				return int.Parse(result);
+
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+
+			}
+		}
+
+		public int GetIndexesCount(string dbname)
+		{
+			string result = "";
+			try
+			{
+				SqlCommand cmd = new SqlCommand("udp_getIndexesCount");
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@dbname", dbname);
+				cmd.Connection = DBconnection.DBConnect();
+				result = cmd.ExecuteScalar().ToString();
+				return int.Parse(result);
+
 			}
 			catch (Exception ex)
 			{
