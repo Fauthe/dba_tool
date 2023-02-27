a8ff9c097266c4f75fc90684518b14f8efd407c3
fauthe <manishlama731@gmail.com>
2023-02-27 17:20:17 +0545

    working on login again

--- dba_tool/Controllers/LoginController.cs
+++ dba_tool/Controllers/LoginController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using dba_tool.Models;
+using dba_tool.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dba_tool.Controllers
 {
 	public class LoginController : Controller
 	{
+		LoginCon lgn = new LoginCon();
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		[HttpPost]
+		public IActionResult Index(LoginCredential lc)
+		{
+			return View(lgn.Credential);
+		}
+
+		public IActionResult VerifyLogin(string username, string password)
+		{
+			if(username==null&&password==null)
+			{
+				TempData["ErrorMsg"] = "Username and Password Field is Empty!!!";
+				return Redirect("~/Login/Index");
+			}
+			if(username == null || password == null)
+			{
+				TempData["ErrorMsg"] = "Username or Password Field is Empty!!!";
+				return Redirect("~/Login/Index");
+			}
+			lgn.checkUsername(username);
+			if (username != null && password != null)
+			{
+				foreach (var item in lgn.Credential)
+				{
+					if (username != null & item.Username == null)
+					{
+						TempData["ErrorMsg"] = "User Does Not Exists";
+						return Redirect("~/Login/Index");
+					}
+					if (item.Username != null)
+					{
+						TempData["ErrorMsg"] = "User Does Exists";
+						return Redirect("~/Login/Index");
+					}
+				}
+				
+			}
+			return View();
+
+
+
+		}
 	}
+
 }
